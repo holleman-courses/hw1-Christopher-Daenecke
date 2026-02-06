3a6ddec18c2fe6a82092cf72592171736a6cb6ae
Christopher Daenecke <cdaenecke@ChrisLaptop.localdomain>
2026-02-06 13:18:54 -0500

    training model 1

--- hw1_complete.py
+++ hw1_complete.py
@@ -21,12 +21,9 @@ print(f"Keras Version: {keras.__version__}")
 def build_model1():
   model = tf.keras.Sequential([
       layers.Flatten(input_shape=(32,32,3)),
-      layers.Dense(128),
-      layers.LeakyReLU(),
-      layers.Dense(128),
-      layers.LeakyReLU(),
-      layers.Dense(128),
-      layers.LeakyReLU(),
+      layers.Dense(128, activation=tf.keras.layers.LeakyReLU()),
+      layers.Dense(128, activation=tf.keras.layers.LeakyReLU()),
+      layers.Dense(128, activation=tf.keras.layers.LeakyReLU()),
       layers.Dense(10)
   ]) 
   
@@ -58,9 +55,31 @@ if __name__ == '__main__':
   ## Add code here to Load the CIFAR10 data set
   (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
   
+  val_frac = 0.1
+  num_val = int(len(train_images) * val_frac)
+
+  val_idxs = np.random.choice(np.arange(len(train_images)), size=num_val, replace=False)
+  trn_idxs = np.setdiff1d(np.arange(len(train_images)), val_idxs)
+
+  train_images = train_images.astype("float32") / 255.0
+  test_images = test_images.astype("float32") / 255.0
+
+  val_images = train_images[val_idxs]
+  val_labels = train_labels[val_idxs]
+  train_images = train_images[trn_idxs]
+  train_labels = train_labels[trn_idxs]
+
   ########################################
   ## Build and train model 1
   model1 = build_model1()
+
+  history1 = model1.fit(
+    train_images,
+    train_labels,
+    epochs = 30,
+    validation_data = (val_images, val_labels),
+    batch_size = 32
+  )
   # compile and train model 1.
 
   ## Build, compile, and train model 2 (DS Convolutions)
